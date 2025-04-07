403a8abf121bd5a9379b6236e45335ad1a2ca9c0
KateriumG <kevinjoelescobar2020@gmail.com>
2025-04-07 13:28:31 -0500

    imagenes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let CreditVisible = false;
+let SolarImages = ["Assets/Cocinas Solares/CocinaSolar1.jpg", "Assets/Cocinas Solares/CocinaSolar2.jpeg", "Assets/Cocinas Solares/CocinaSolar3.jpeg", "Assets/Cocinas Solares/CocinaSolar4.jpeg", "Assets/Cocinas Solares/CocinaSolar5.webp"];
+let SolarIndex = 0;
 
 function ChangeCreditsV() {
     var credits = document.getElementById("Credits");
@@ -12,4 +14,8 @@ function ChangeCreditsV() {
         CreditVisible = false;
         creditsbutton.style.marginBottom = "250px";
     }
+}
+
+function ChangeSolarIndex(number){
+    
 }
